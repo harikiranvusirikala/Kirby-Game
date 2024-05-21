32213095efabfc3276775a1829803c76c2c80696
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2024-05-21 14:07:18 +0530

    Displaying the level

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,7 @@ import { scale } from "./constants";
 export async function makeMap(k: KaboomCtx, name: string) {
     const mapData = await (await fetch(`./${name}.json`)).json();
 
-    const map = k.make([k.sprite("name "), k.scale(scale), k.pos(0)]);
+    const map = k.make([k.sprite(name), k.scale(scale), k.pos(0)]);
 
     const spawnPoints: { [key: string]: { x: number, y: number }[] } = {};
 

3df416c107e18cb31026a29fad96797a9b70ea25
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2024-05-22 14:51:58 +0530

    Create and add guy enemies to the scene

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { makeFlameEnemy, makePlayer, setControls } from "./entities";
+import { makeFlameEnemy, makeGuyEnemy, makePlayer, setControls } from "./entities";
 import { k } from "./kaboomCtx";
 import { makeMap } from "./utils";
 
@@ -50,8 +50,12 @@ async function gameSetup() {
                 k.camPos(kirb.pos.x + 500, 800);
         });
 
-        for(const flame of level1SpawnPoints.flame) {
+        for (const flame of level1SpawnPoints.flame) {
             makeFlameEnemy(k, flame.x, flame.y);
+        }
+
+        for (const guy of level1SpawnPoints.guy) {
+            makeGuyEnemy(k, guy.x, guy.y);
         }
     })
 
